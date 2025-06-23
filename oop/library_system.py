3fe8416ebc133d01e13103d1c66247b6c4887706
petermriya <110281894+petermriya@users.noreply.github.com>
2025-06-23 17:30:27 +0000

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,28 +3,39 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        Book.__init__(self, title, author)
+        super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
-        Book.__init__(self, title, author)
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        self.books.append(book)
-
+        if isinstance(book, Book):
+            self.books.append(book)
+        
     def list_books(self):
-        for book in self.books:
-            if isinstance(book, EBook):
-                print("E-Book:", book.title, "by", book.author, "-", book.file_size, "MB")
-            elif isinstance(book, PrintBook):
-                print("Print Book:", book.title, "by", book.author, "-", book.page_count, "pages")
-            else:
-                print("Book:", book.title, "by", book.author)
+        if not self.books:
+            print("The library has no books.")
+        else:
+            for book in self.books:
+                print(book)
